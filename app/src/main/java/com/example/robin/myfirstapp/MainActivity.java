a3e388cf30b6fe0065f69d071ebb9116824fc939
Hackerlacka <robin.robbe96@gmail.com>
2018-03-22 11:57:41 +0100

    Compass working

--- app/src/main/java/com/example/robin/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/robin/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.robin.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startCompass(View view) {
+        Intent intent = new Intent(this, CompassActivity.class);
+        // TODO Optional parameters
+        startActivity(intent);
+    }
+
+    public void startAccelerometers(View view) {
+        Intent intent = new Intent(this, AccelerometersActivity.class);
+        startActivity(intent);
     }
 }
